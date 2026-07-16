3783e6f6372ef26732ed69cec1367c8e82d48383
Prateek Bahad <prateekbahad@gmail.com>
2026-07-16 16:51:25 +0530

    update server

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ dotenv.config();
 connectDB()
 const PORT = process.env.SERVER_PORT;
 const ADMIN_PORT = process.env.ADMIN_SIDE_PORT
+const RENDER_ADMIN_URL=process.env.RENDER_URL
 
 const app = express();
 // MIDDLEWARE
 app.use(express.json());
 app.use(cors({
-  origin:(`http://localhost:${ADMIN_PORT}`)
+  origin:(`${RENDER_ADMIN_URL} || http://localhost:${ADMIN_PORT}`)
 }));
 
 // ROUTER
