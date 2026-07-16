abe3f6dad73e212919448bf430282f59a24a3639
Prateek Bahad <prateekbahad@gmail.com>
2026-07-16 16:58:04 +0530

    update cors

--- server.js
+++ server.js
@@ -2,25 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
-import userRoute from "./routes/UserRoute.js"
+import userRoute from "./routes/UserRoute.js";
 
 // DOTENV CONFIG
 dotenv.config();
-connectDB()
+connectDB();
 const PORT = process.env.SERVER_PORT;
-const ADMIN_PORT = process.env.ADMIN_SIDE_PORT
-const RENDER_ADMIN_URL=process.env.RENDER_URL
+// const ADMIN_PORT = process.env.ADMIN_SIDE_PORT;
+const RENDER_ADMIN_URL = process.env.RENDER_URL;
 
 const app = express();
 // MIDDLEWARE
 app.use(express.json());
-app.use(cors({
-  origin:(`${RENDER_ADMIN_URL} || http://localhost:${ADMIN_PORT}`)
-}));
+app.use(
+  cors({
+    origin: RENDER_ADMIN_URL,
+  }),
+);
 
 // ROUTER
-app.use("/api/v1/employee",userRoute)
-
+app.use("/api/v1/employee", userRoute);
 
 // SERVER IS RUNNING
 app.listen(PORT, () => {
